aa895684558a9d11b32a2f8f60f50e1bc7daddd8
Michelle Jowers <michelle.jowers@icloud.com>
2021-10-13 16:28:22 -0400

    update request to work with both java & c#

--- PrsWebApi2/Controllers/RequestsController.cs
+++ PrsWebApi2/Controllers/RequestsController.cs
@@ -8,35 +8,29 @@ using Microsoft.EntityFrameworkCore;
 using PrsWebApi2.Data;
 using PrsWebApi2.Models;
 
-namespace PrsWebApi2.Controllers
-{
+namespace PrsWebApi2.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class RequestsController : ControllerBase
-    {
+    public class RequestsController : ControllerBase {
         private readonly AppDbContext _context;
 
-        public RequestsController(AppDbContext context)
-        {
+        public RequestsController(AppDbContext context) {
             _context = context;
         }
 
         // GET: api/Requests
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Request>>> GetRequest()
-        {
+        public async Task<ActionResult<IEnumerable<Request>>> GetRequest() {
             return await _context.Requests.Include(r => r.user).ToListAsync();
         }
 
         // GET: api/Requests/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Request>> GetRequest(int id)
-        {
+        public async Task<ActionResult<Request>> GetRequest(int id) {
             var request = await _context.Requests.Include(r => r.user)
                 .SingleOrDefaultAsync(r => r.Id == id);
 
-            if (request == null)
-            {
+            if (request == null) {
                 return NotFound();
             }
 
@@ -45,8 +39,7 @@ namespace PrsWebApi2.Controllers
 
         // GET: api/Requests
         [HttpGet("request-lines/{id}")]
-        public async Task<ActionResult<IEnumerable<Request>>> GetRequestByStatus(int id)
-        {
+        public async Task<ActionResult<IEnumerable<Request>>> GetRequestByStatus(int id) {
             return await _context.Requests.Where(req => req.Status == "review" && req.UserId != id)
                 .Include(r => r.user).ToListAsync();
         }
@@ -55,41 +48,37 @@ namespace PrsWebApi2.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutRequest(int id, Request request)
-        {
-            if (id != request.Id)
-            {
+        public async Task<IActionResult> PutRequest(int id, Request request) {
+            if (id != request.Id) {
                 return BadRequest();
             }
 
             _context.Entry(request).State = EntityState.Modified;
 
-            try
-            {
+            try {
                 await _context.SaveChangesAsync();
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RequestExists(id))
-                {
+            catch (DbUpdateConcurrencyException) {
+                if (!RequestExists(id)) {
                     return NotFound();
                 }
-                else
-                {
+                else {
                     throw;
                 }
             }
-
             return NoContent();
+        }
+
+        [HttpPut("submit-review")]
+        public async Task<IActionResult> PutRequestReview(Request request) {
+            return await PutRequestReview(request.Id, request);
         }
 
         // submit a change for the request approve if < 50.00 otherwise set to 'review'
         [HttpPut("submit-review/{id}")]
-        public async Task<IActionResult> PutRequestReview(int id, Request request)
-        {
+        public async Task<IActionResult> PutRequestReview(int id, Request request) {
 
-            if (request.Total <= 50)
-            {
+            if (request.Total <= 50) {
                 request.Status = "Approved";
             }
             else { request.Status = "Review"; }
@@ -105,13 +94,12 @@ namespace PrsWebApi2.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
-        public async Task<ActionResult<Request>> PostRequest(Request request)
-        {
+        public async Task<ActionResult<Request>> PostRequest(Request request) {
             request.user = null;
 
             request.Status = "new";
             request.SubmittedDate = DateTime.Now;
-            
+
             _context.Requests.Add(request);
             await _context.SaveChangesAsync();
 
@@ -120,11 +108,9 @@ namespace PrsWebApi2.Controllers
 
         // DELETE: api/Requests/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Request>> DeleteRequest(int id)
-        {
+        public async Task<ActionResult<Request>> DeleteRequest(int id) {
             var request = await _context.Requests.FindAsync(id);
-            if (request == null)
-            {
+            if (request == null) {
                 return NotFound();
             }
 
@@ -134,22 +120,19 @@ namespace PrsWebApi2.Controllers
             return request;
         }
 
-        private bool RequestExists(int id)
-        {
+        private bool RequestExists(int id) {
             return _context.Requests.Any(e => e.Id == id);
         }
         // put: change record without calling id number
         [HttpPut]
-        public async Task<IActionResult> PutRequest(Request request)
-        {
+        public async Task<IActionResult> PutRequest(Request request) {
             _context.Entry(request).State = EntityState.Modified;
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
         [HttpPut("approve")]
-        public async Task<ActionResult<Request>> PutRequestApprove(Request request)
-        {
+        public async Task<ActionResult<Request>> PutRequestApprove(Request request) {
 
             request.Status = "Approved";
 
@@ -159,8 +142,7 @@ namespace PrsWebApi2.Controllers
             return request;
         }
         [HttpPut("reject")]
-        public async Task<ActionResult<Request>> PutRequestReject(Request request)
-        {
+        public async Task<ActionResult<Request>> PutRequestReject(Request request) {
 
             request.Status = "Rejected";
 
